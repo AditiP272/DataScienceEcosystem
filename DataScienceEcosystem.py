fbe8c159f4519c0782afc66c5433468051af788c
AditiP272 <142074653+AditiP272@users.noreply.github.com>
2023-08-12 10:59:32 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ### In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ### Some of the key takeaways from the course
+
+# **Objectives:**
+# - List of popular languages for Data Science.
+# - List examples of commercial and cloud-based data science tools.
+# - Define SQL elements and list its benefits.
+# - List the various libraries: scientific, visualization, machine learning, and deep learning.
+# - Describe how to use the notebooks in JupyterLab.
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R Language
+# 3. SQL
+# 4. MATLAB
+# 5. SAS
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1. numpy
+# 2. pandas
+# 3. scikit-learn
+# 4. matplotlib
+
+# | Data Science Tools | 
+# |:----------|
+# | Jupyter Notebook   | 
+# | R Studio   | 
+# | Visual Studio Code  |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python:
+
+# In[4]:
+
+
+#  This a simple arithmetic expression to mutiply then add integers
+out=(3*4)+5
+print(out)
+
+
+# In[5]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+minutes = 200
+hours = minutes / 60
+
+print(hours)
+
+
+# ## Author
+# Aditi Pradhan
